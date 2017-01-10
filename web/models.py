bb6fdaa4416f1e2def62818aed6301a3471cef31
Homa-Amini <amini1127@gmail.com>
2017-01-10 16:15:58 +0100

    submit/expense and submit/income web services

--- web/models.py
+++ web/models.py
@@ -4,6 +4,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    token = models.CharField(max_length = 48)
+    def __unicode__(self):
+        return "{}_token".format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length = 255)
     date = models.DateTimeField()
@@ -11,6 +17,7 @@ class Expense(models.Model):
     user = models.ForeignKey(User)
     def __unicode__(self):
         return "{}-{}".format(self.date,self.amount)
+
 class Income(models.Model):
     text = models.CharField(max_length = 255)
     date = models.DateTimeField()
--- web/views.py
+++ web/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+from web.models import User, Token, Expense, Income
 
 # Create your views here.
+@csrf_exempt
+def submit_income(request):
+     """user submits an income"""
+
+     #TODO; validate data, user might be fake, token might be fake, amount might be in  awrong format ...
+     this_token = request.POST['token']
+     this_user = User.objects.filter(token__token = this_token).get()
+     if 'date' not in request.POST:
+         date = datetime.now()
+
+     Income.objects.create(
+        user = this_user,
+        amount = request.POST['amount'],
+        text = request.POST['text'],
+        date = date
+     )
+
+     return JsonResponse(
+        {'status':'OK',},
+        encoder=JSONEncoder
+    )
+
+@csrf_exempt
+def submit_expense(request):
+     """user submits an expense"""
+
+     #TODO; validate data, user might be fake, token might be fake, amount might be in  awrong format ...
+     this_token = request.POST['token']
+     this_user = User.objects.filter(token__token = this_token).get()
+     if 'date' not in request.POST:
+         date = datetime.now()
+
+     Expense.objects.create(
+        user = this_user,
+        amount = request.POST['amount'],
+        text = request.POST['text'],
+        date = date
+     )
+
+     return JsonResponse(
+        {'status':'OK',},
+        encoder=JSONEncoder
+    )
